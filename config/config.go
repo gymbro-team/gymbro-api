086d0ea6d5b88fe1eec69fb69816dee1a3414790
Diógenes Dietrich <diogenes.dietrich.dev@gmail.com>
2024-04-06 12:58:24 -0300

    create config file for retrive env variables

---
+++ config/config.go
@@ -0,0 +1,46 @@
+package config
+
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+type DatabaseConfig struct {
+	User     string
+	Password string
+	Name     string
+}
+
+type Config struct {
+	Database      DatabaseConfig
+	ServerAddress string
+}
+
+func getEnv(key, defaultValue string) string {
+	value, ok := os.LookupEnv(key)
+
+	if !ok {
+		return defaultValue
+	}
+
+	return value
+}
+
+func LoadConfig() *Config {
+	if err := godotenv.Load(); err != nil {
+		log.Fatalf("Error loading .env file: %v", err)
+	}
+
+	dbConfig := DatabaseConfig{
+		User:     getEnv("DB_USER", ""),
+		Password: getEnv("DB_PASSWORD", ""),
+		Name:     getEnv("DB_NAME", ""),
+	}
+
+	return &Config{
+		Database:      dbConfig,
+		ServerAddress: getEnv("SERVER_ADDRESS", ":3000"),
+	}
+}
