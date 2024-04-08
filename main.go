7c862f4b51fa5bc6c5034aab0db1da00dfb8ebc2
Diógenes Dietrich <diogenes.dietrich.dev@gmail.com>
2024-04-08 16:20:36 -0300

    removed unecessary comments

--- main.go
+++ main.go
@@ -14,32 +14,22 @@ import (
 )
 
 func main() {
-	// Load configuration
 	cfg := config.LoadConfig()
 
-	// Connect to the database
 	db, err := sql.Open("postgres", "user="+cfg.Database.User+" password="+cfg.Database.Password+" dbname="+cfg.Database.Name+" sslmode=disable")
 	if err != nil {
 		log.Fatalf("Failed to connect to the database: %v", err)
 	}
 	defer db.Close()
 
-	// Create repository instances
 	userRepo := repository.NewUserRepository(db)
-
-	// Create controller instances
 	userController := controller.NewUserController(userRepo)
-
-	// Create handler instances
 	userHandler := handler.NewUserHandler(userController)
 
-	// Create a ServeMux (router)
 	mux := mux.NewRouter()
 
-	// Register user-related routes
 	userHandler.RegisterRoutes(mux)
 
-	// Start the HTTP server
 	addr := cfg.ServerAddress
 	log.Printf("Server listening on %s", addr)
 	if err := http.ListenAndServe(addr, mux); err != nil {
