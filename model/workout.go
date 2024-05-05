07de10b43d001ac5d9797a634db196658b32a745
Diógenes Dietrich <diogenes.dietrich.dev@gmail.com>
2024-05-05 14:31:08 -0300

    added exercises array to workout struct

--- model/workout.go
+++ model/workout.go
@@ -3,16 +3,17 @@ package model
 import "time"
 
 type Workout struct {
-	ID             uint64    `json:"id"`
-	AthleteID      *uint64   `json:"athlete_id"`
-	PersonalID     *uint64   `json:"personal_id"`
-	Name           string    `json:"name"`
-	Icon           string    `json:"icon"`
-	CoverImage     string    `json:"cover_image"`
-	WeekDay        byte      `json:"week_day"`
-	SetsCount      int       `json:"sets_count"`
-	RepsCount      int       `json:"reps_count"`
-	ExercisesCount int       `json:"exercises_count"`
-	CreatedAt      time.Time `json:"created_at"`
-	UpdatedAt      time.Time `json:"updated_at"`
+	ID             uint64     `json:"id"`
+	AthleteID      *uint64    `json:"athlete_id"`
+	PersonalID     *uint64    `json:"personal_id"`
+	Name           string     `json:"name"`
+	Icon           string     `json:"icon"`
+	CoverImage     string     `json:"cover_image"`
+	WeekDay        byte       `json:"week_day"`
+	SetsCount      int        `json:"sets_count"`
+	RepsCount      int        `json:"reps_count"`
+	ExercisesCount int        `json:"exercises_count"`
+	Exercises      []Exercise `json:"exercises"`
+	CreatedAt      time.Time  `json:"created_at"`
+	UpdatedAt      time.Time  `json:"updated_at"`
 }
